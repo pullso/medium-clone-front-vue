ecba21b23f0d17c322bb844e134bd2b6bccc048c
pullso <pullso.code@gmail.com>
2021-08-07 23:04:26 +0300

    add FeedToggle.vue

---
+++ src/components/FeedToggle.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="feed-toggle">
+    <ul class="nav nav-pills outline-active">
+      <li class="nav-item" v-if="isLoggedIn">
+        <router-link :to="{ name: 'yourFeed' }" class="nav-link">
+          Your feed
+        </router-link>
+      </li>
+      <li class="nav-item">
+        <router-link :to="{ name: 'globalFeed' }" class="nav-link">
+          Global feed
+        </router-link>
+      </li>
+      <li class="nav-item" v-if="tagName">
+        <router-link :to="{ name: 'tag' }" class="nav-link">
+          <i class="ion-pound" />
+          {{ tagName }}
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import { getterTypes } from "@/store/modules/auth";
+
+export default {
+  name: "AppFeedToggle",
+  props: {
+    tagName: {
+      type: String,
+      required: false,
+    },
+  },
+  computed: {
+    ...mapGetters({
+      isLoggedIn: getterTypes.isLoggedIn,
+    }),
+  },
+};
+</script>
